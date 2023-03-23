6b575e4139e81b2d196a83e6e6bc16903af20379
Nicholas <nicholasmoskalenko@gmail.com>
2023-03-23 21:29:48 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  if (delay.value < 0 || step.value < 0 || amount.value <= 0) {
+    Notify.failure('Please fill with positive numbers!');
+    return;
+  }
+
+  delay = Number(delay.value);
+  console.log(delay);
+  for (let position = 1; position <= amount.value; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(step.value);
   }
 }
